c1222c7be78cbd0ae896ac94d16076482e96b480
Martin Prpič <martin.prpic@gmail.com>
2018-05-11 16:20:24 +0200

    Refactor some of the old Python code
    Also, get rid of now unused SVG file and remove Jinja dependency. It was
    only used to insert two strings, something that a simple regex can do
    just as well.

--- README.md
+++ README.md
@@ -5,6 +5,13 @@ Create a single-page HTML gallery with:
 
     $ ./create-gallery.py -i vacation-photos -o gallery
 
-where `vacation-photos` is the input directory and `gallery` is the output directory. The script first searches for all image files within the input directory and then creates the output directory with a generated `index.html` file, two SVG files, and all image files found in the input directory.
+where `vacation-photos` is the input directory and `gallery` is the output
+directory. The script first searches for all image files within the input
+directory and then creates the output directory with a generated
+`index.html` file and all image files found in the input directory.
 
-The created directory (in this case, `./gallery`) can then be moved into your document root (e.g. `/var/www/html/`).
+Optionally, you can specify a title for the generated page using the
+`--title` option.
+
+The created directory (in this case, `./gallery`) can then be moved into
+your document root (e.g. `/var/www/html/`).
--- create-gallery.py
+++ create-gallery.py
@@ -1,77 +1,76 @@
 import os
+import re
 import sys
 import shutil
-import optparse
-try:
-    from jinja2 import Environment, FileSystemLoader
-except ImportError:
-    print("ERROR: missing jinja2 module")
-    sys.exit(1)
+import argparse
 
-GALLERY_TEMPLATE = './gallery.html'
+GALLERY_TEMPLATE = os.path.join(os.path.dirname(__file__), './gallery.template')
 IMAGE_EXT = ['.png', '.jpg', '.jpeg']
+IMAGE_HTML = '<img class="hidden" data-src="{image}" alt="Loading photo..." />'
 
 
 def main():
+    p = argparse.ArgumentParser(description='Generate a simple one-page HTML gallery.')
+    p.add_argument('-i', '--input', dest='in_dir', required=True,
+                   action='store', help='input directory where images are located')
+    p.add_argument('-o', '--output', dest='out_dir',
+                   action='store', help='output directory (default: "./gallery")')
+    p.add_argument('-t', '--title', dest='title',
+                   action='store', help='title to be used in the <title> tag of the HTML (default: Gallery)')
+    args = p.parse_args()
 
-    p = optparse.OptionParser(description='Generate a simple one-page HTML gallery.',
-                              usage='%prog [-h|--help] -i DIR [-o OUTDIR]')
-    p.add_option('-i', '--input', dest='in_dir',
-                 action='store', help='Input directory where images are located')
-    p.add_option('-o', '--output', dest='out_dir',
-                 action='store', help='Output directory (default: "./gallery")')
-    p.add_option('-t', '--title', dest='title',
-                 action='store', help="Title to be used in the gallery's HTML (default: Gallery)")
-    (opts, args) = p.parse_args()
-
-    if not opts.in_dir:
+    if not args.in_dir:
         p.error('You must specify an input directory!')
         sys.exit(1)
 
-    if not os.path.isdir(opts.in_dir):
+    if not os.path.isdir(args.in_dir):
         p.error('The specified location is not a valid directory')
 
     # Default title is 'Gallery'
-    title = opts.title if opts.title else 'Gallery'
+    title = args.title if args.title else 'Gallery'
 
     # Default output is './gallery'
-    out_dir = opts.out_dir.rstrip('/') if opts.out_dir else './gallery'
+    out_dir = args.out_dir.rstrip('/') if args.out_dir else './gallery'
 
     # Overwriting is dangerous, quitting is safer
     if os.path.exists(out_dir):
-        print('ERROR: Directory %s already exists. Remove it or specify a different one.' % out_dir)
+        print('ERROR: Directory {} already exists. Remove it or specify a different one.'.format(out_dir))
         sys.exit(1)
 
-    file_list = os.listdir(opts.in_dir)
+    file_list = os.listdir(args.in_dir)
 
     # Filter out non-image files
     img_file_list = []
     for f in file_list:
-        if os.path.splitext(f)[1].lower() in IMAGE_EXT:
+        _, file_ext = os.path.splitext(f)
+        if file_ext.lower() in IMAGE_EXT:
             img_file_list.append(f)
         else:
-            print('Skipping %s -- not an image file' % f)
+            print('Skipping {} -- not an image file'.format(f))
 
     if not img_file_list:
-        print('ERROR: Did not locate any images files the specified directory')
+        print('ERROR: Did not locate any image files in {}'.format(args.in_dir))
         sys.exit(1)
 
-    # Load up Jinja template and pass sorted data
-    jinja_env = Environment(loader=FileSystemLoader('./'))
-    template = jinja_env.get_template(GALLERY_TEMPLATE)
-    rendered_html = template.render(title=title, images=sorted(img_file_list))
+    # Open gallery.html template and add images and title
+    with open(GALLERY_TEMPLATE) as f:
+        template = f.read()
+
+    html = re.sub(r'{{ TITLE }}', title, template)
+    photos_html = [IMAGE_HTML.format(image=image) for image in sorted(img_file_list)]
+    html = re.sub(r'{{ PHOTOS }}', '\n'.join(photos_html), html)
 
     # Create output directory
-    print('Creating directory: %s' % out_dir)
+    print('Created directory: {}'.format(out_dir))
     os.makedirs(out_dir)
 
     # Write out HTML file
     with open(os.path.join(out_dir, 'index.html'), 'w') as f:
-        f.write(rendered_html)
+        f.write(html)
 
     # Copy all image files
     for f in img_file_list:
-        shutil.copy('%s/%s' % (opts.in_dir.rstrip('/'), f), out_dir)
+        shutil.copy('{}/{}'.format(args.in_dir.rstrip('/'), f), out_dir)
 
 
 if __name__ == '__main__':
